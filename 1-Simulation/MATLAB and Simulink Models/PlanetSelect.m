fdc223c6cdd62cb00b894089f8836798e59be395
TheGreatFez <raul.maldonado@mavs.uta.edu>
2017-04-10 10:24:01 -0500

    Shared Folder and Simulink Upload
    feat: Added new shared scripts folder for all to add their stuff to
    incase you want to share.
    
    feat: Added my simulation model derived for this purpose. It is
    currently 2D
    
    maint: Added some clarity to the coding standards doc

---
+++ 1-Simulation/MATLAB and Simulink Models/PlanetSelect.m
@@ -0,0 +1,62 @@
+Planet = 'Kerbin'; % Choice between Earth and Mars
+%load 'DragData_3manCap.mat'
+load 'DragData_TwoStageTestCraft.mat'
+load 'EarthAtmoDataRSS.mat'
+load 'Mars Atmo Data.mat'
+load 'KerbinAtmoData.mat'
+%load 'ThrustMassData.mat'
+load 'TwoStageTestCraft.mat'
+Max = 80000;
+LastStage = size(ThrustMassData,1);
+g0 = 9.80665; % m/s^2 standard gravity
+q0 = 1.1577; % kg/m^3
+Min_Throttle = 1.1*g0*ThrustMassData(1,1)/(1000*ThrustMassData(1,3));
+Max_Throttle = min(1,3*g0*ThrustMassData(1,1)/(1000*ThrustMassData(1,3)));
+K1 = 1.5;
+K2 = .825;
+
+
+GravityTurn = 1;
+PitchOverAlt = 1000;
+PitchOverAngle = 7.5;
+Throttle = 1.7*g0*ThrustMassData(1,1)/(1000*ThrustMassData(1,3));
+SSIGN = 306.412;
+PitchSwitch = .5;
+
+PitchProgram = PitchProgramSet(GravityTurn,PitchOverAlt,PitchOverAngle);
+
+[R_planet,GndAlt_planet,Omega_planet,AtmoAlt_planet,GM_planet,Planet] = PlanetParameters(Planet);
+
+if strcmp('Earth',Planet) == 1
+    AtmoData = EarthAtmoDataRSS;
+elseif strcmp('Mars',Planet) == 1
+    AtmoData = MarsAtmoDataRSS;
+elseif strcmp('Kerbin',Planet) == 1
+    AtmoData = KerbinAtmoData;
+else
+    AtmoData = 0;   
+end
+
+TargetOrbit = 100000-GndAlt_planet; % m
+
+entry_angle = 6.5; % degrees
+entry_speed = 11000; % m/s
+
+Px = 0; % m
+Py = R_planet+.1; % m
+
+X = [Px;Py;0];
+Y = [0;0;Omega_planet];
+Z = cross(X,Y);
+
+Vx = Z(1); % m/s
+Vy = Z(2); % m/s
+
+CraftInitialV = [Vx;Vy];
+CraftInitialP = [Px;Py];
+
+RefSurfArea = 10.8; % m^2
+Cd = 2;
+%Mass = 5600; % kg
+%WetMass = 7100; % kg
+%DryMass = 3100; % kg 
---
+++ 9-Shared_Scripts/README.txt
@@ -0,0 +1,5 @@
+SHARED SCRIPTS:
+
+When adding files here, these can be your own personal files. Please create a folder with your username (from reddit) to keep it organized. 
+
+As far as the contents of the folder, as long as someone can find it you can keep it as organized as you like
